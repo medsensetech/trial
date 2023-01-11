80a9ead8b48b8db3754c8453fccd51a11e4a1ad3
Dhairya Vayada <dhairya.vayada@gmail.com>
2023-01-11 18:10:57 +1100

    Update app.py

--- app.py
+++ app.py
@@ -4,27 +4,22 @@ if __name__ == "__main__":
     # setting header, description and citation
     st.set_page_config(page_title="Molecule icons")
     st.header('''
-    Molecule Icon Generator!
+    Evidence Explorer
     ''')
     st.write('''
-    Generate icons of molecules from SMILES, Name, Cas-number, Inchi, InChIKey, load your molecule file or convert a
-    list of SMILES.
+    Enter your search criteria in the fields below then click on Results to explore the published data available that
+most closely matches your selections.
+The results would display the services and channels used by similar programs along with the expected adoption
+rates and outcomes if you were to design your program similarly.
     ''')
-    st.markdown('''
-For more options and information, check out the 
-[GitHub repository](https://github.com/lmonari5/molecule-icon-generator.git).\\
-[DOI](https://doi.org/10.5281/zenodo.7388429): 10.5281/ZENODO.7388429.
-       ''')
-
-    # select the input type
-    input_type = st.selectbox("Create your icon by",
-                              ['name', 'smiles', 'load file', 'cas_number', 'stdinchi', 'stdinchikey', 'smiles list'],
-                              on_change=updatemol,
-                              help="""Choose the input info of your molecule. If the app is slow, use SMILES input.""" + smiles_help)
-    # default input for each input_type except 'load file'
-    def_dict = {'name': 'paracetamol',
-                'smiles': "CC(=O)Nc1ccc(cc1)O",
-                'cas_number': '103-90-2',
-                'stdinchi': 'InChI=1S/C8H9NO2/c1-6(10)9-7-2-4-8(11)5-3-7/h2-5,11H,1H3,(H,9,10)',
-                'stdinchikey': 'RZVAJINKPMORJF-UHFFFAOYSA-N'}
+    
+    with st.form('Form1'):
+        st.selectbox('Therapy Area', ['Vanilla', 'Chocolate'], key=1)
+        st.selectbox('Condition', ['Vanilla', 'Chocolate'], key=1)
+        st.selectbox('Therapy Area', ['Vanilla', 'Chocolate'], key=1)
+        st.selectbox('Drug Name', ['Vanilla', 'Chocolate'], key=1)
+        st.selectbox('Program Stakeholders', ['Vanilla', 'Chocolate'], key=1)
+        st.selectbox('Program Strategy', ['Vanilla', 'Chocolate'], key=1)
+        st.selectbox('Route of Administration', ['Vanilla', 'Chocolate'], key=1)        
+        submitted1 = st.form_submit_button('Submit 1')
 
