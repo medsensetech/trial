5f7a873e751e9d6777f5e61dcf10c796b18e7b11
Dhairya Vayada <dhairya.vayada@gmail.com>
2023-01-11 18:12:48 +1100

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,6 @@ rates and outcomes if you were to design your program similarly.
         st.selectbox('Route of Administration', ['Vanilla', 'Chocolate'], key=1)        
         submit_button = st.form_submit_button('Submit 1')
 
-form = st.form(key='my_form')
-form.text_input(label='Enter some text')
-submit_button = form.form_submit_button(label='Submit')
+    form = st.form(key='my_form')
+    form.text_input(label='Enter some text')
+    submit_button = form.form_submit_button(label='Submit')
