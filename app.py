893584d2fd8a4b13ed2b29c3ba525c2e3ce37905
Dhairya Vayada <dhairya.vayada@gmail.com>
2023-01-19 22:22:02 +1100

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,8 @@ rates and outcomes if you were to design your program similarly.
     st.header("Concept Builder")
     
     st.write("Based on selection(s) of program strategy, for your program objective(s) might be recommended below. For strategy around either un-met need or barrier or existing-programs please select any relevant objectives. Also, complete the additional felds below, then press next, to review the potential opportunity for your program concept.")
-        
+    
+    st.write(tag)
 
     tag = []
     #print(program_strategy)
