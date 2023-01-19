7533e959b301096de37cc272021ed3043ad9e0ac
Dhairya Vayada <dhairya.vayada@gmail.com>
2023-01-19 19:34:56 +1100

    Update app.py

--- app.py
+++ app.py
@@ -735,9 +735,9 @@ rates and outcomes if you were to design your program similarly.
     st.subheader('ALT1 Scenario')
     with st.form('Form4'):
         c38, c39, c40 = st.columns(3)
-        alt1_ar = c38.number_input("ALT1 - Adoption Rate")
-        alt1_ygp = c39.number_input("ALT1 - Yearly growth Program")
-        alt1_au = c40.number_input("ALT1 - Additional units per pt per yr")
+        alt1_ar = c38.number_input("ALT1 - Adoption Rate", value=adoption_1)
+        alt1_ygp = c39.number_input("ALT1 - Yearly growth Program", value=annual_growth)
+        alt1_au = c40.number_input("ALT1 - Additional units per pt per yr", value=ca_6)
         alt1_output = ca(alt1_ar, alt1_ygp, alt1_au)
         submit_button4 = st.form_submit_button('Go')
     if submit_button4:
